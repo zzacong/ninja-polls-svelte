620774c1afe3db69c7737ac8774fb56cad76a4bd
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-01-09 16:39:22 +1100

    tutorial: tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,27 @@
 <script>
   import Header from './components/Header.svelte'
   import Footer from './components/Footer.svelte'
+  import Tabs from './shared/Tabs.svelte'
+
+  // tabs
+  let items = ['Current polls', 'New poll']
+  let activeItem = 'Current polls'
+
+  const handleTabChange = ({ detail }) => {
+    activeItem = detail
+  }
 </script>
 
 <Header />
-<main />
+<main>
+  <Tabs {items} {activeItem} on:tabChange={handleTabChange} />
+
+  {#if activeItem === 'Current polls'}
+    <p>Poll list component goes here</p>
+  {:else if activeItem === 'New poll'}
+    <p>New poll form component goes here</p>
+  {/if}
+</main>
 <Footer />
 
 <style>
---
+++ src/shared/Tabs.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+  export let items, activeItem
+
+  const dispatch = createEventDispatcher()
+</script>
+
+<div class="tabs">
+  <ul>
+    {#each items as item}
+      <li on:click={() => dispatch('tabChange', item)}>
+        <div class:active={item === activeItem}>{item}</div>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .tabs {
+    margin-bottom: 40px;
+  }
+
+  ul {
+    display: flex;
+    gap: 2rem;
+    justify-content: center;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  li {
+    font-size: 18px;
+    color: #555;
+    cursor: pointer;
+  }
+
+  .active {
+    color: #d91b42;
+    border-bottom: 2px solid #d91b42;
+    padding-bottom: 8px;
+  }
+</style>
