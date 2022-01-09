915666449f917da3718189f3dd900a99c04e2066
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-01-09 16:26:56 +1100

    tutorial: header and footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,15 @@
 <script>
+  import Header from './components/Header.svelte'
+  import Footer from './components/Footer.svelte'
 </script>
 
-<main>
-  <h1>Hello Ninjas!</h1>
-</main>
+<Header />
+<main />
+<Footer />
 
 <style>
+  main {
+    max-width: 60rem;
+    margin: 2.5rem auto;
+  }
 </style>
---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<footer>
+  <div class="copyright">Copyright 2022 Poll Ninja</div>
+</footer>
+
+<style>
+  footer {
+    padding: 2.5rem;
+    text-align: center;
+  }
+
+  .copyright {
+    color: #aaaaaa;
+    font-size: 14px;
+    display: inline-block;
+    padding: 20px;
+    border-top: 1px solid #ddd;
+  }
+</style>
